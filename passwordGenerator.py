d96c2897e4294218160e0391d38a560fb09b7345
Greg Stickle <gregorystickle@gmail.com>
2023-03-30 20:47:51 -0400

    Added passwordGenerator

---
+++ passwordGenerator.py
@@ -0,0 +1,31 @@
+#! /usr/bin/env python3
+
+# Welcome to my password Generator!
+
+import random
+import string
+import pyperclip
+
+def generate_password(length=15):
+    password = []
+    for _ in range(length):
+        choice = random.choices(
+            [getNumber(), getLetter(), getCharacter()], 
+            weights=[3, 6, 1], # Increase weights to increase frequency of a character
+            k=1
+        )
+        password.append(choice[0])
+    return ''.join(password)
+
+def getNumber():
+    return random.choice(string.digits)
+
+def getLetter():
+    return random.choice(string.ascii_letters)
+
+def getCharacter():
+    return random.choice('!#$&*?!')
+
+password = generate_password()
+print("Your password has been copied to your clipboard!")
+pyperclip.copy(password)
